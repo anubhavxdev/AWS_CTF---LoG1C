6dad1bb5b6361638a03143456ab745253347a305
Anubhav Jaiswal <anubhav.jaiswal23@lpu.in>
2025-08-31 17:15:08 +0530

    Implement code changes to enhance functionality and improve performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media.licdn.com', 'avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
 };
 
--- src/app/Tablet-scroll/Hero-scroll.js
+++ src/app/Tablet-scroll/Hero-scroll.js
@@ -4,26 +4,38 @@ import { ContainerScroll } from "../components/ui/container-scroll-animation";
 
 export function HeroScrollDemo() {
   return (
-    <div className="flex flex-col overflow-hidden">
-      <ContainerScroll
-        titleComponent={
-          <>
-            <h1 className="text-4xl font-semibold text-black dark:text-white">
-              Meet The Cloud Legends <br />
-              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                 AWS CLOUD TEAM
-              </span>
-            </h1>
-          </>
-        }>
-        <img
-          src="https://media.licdn.com/dms/image/v2/D5622AQHmQhYAeIU_Bg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1719095175154?e=1759363200&v=beta&t=wBK1o_CRdQ-DHIkCsv6ecst0NsRlla-Y2uxX9-AodJg"
-          alt="hero"
-          height={720}
-          width={1400}
-          className="mx-auto rounded-2xl object-cover h-full object-left-top"
-          draggable={false} />
-      </ContainerScroll>
+    <div className="flex flex-col overflow-hidden relative">
+      {/* Background image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center z-0"
+        style={{ backgroundImage: "url('/team2025.jpg')" }}
+      />
+      {/* Black overlay */}
+      <div className="absolute inset-0 bg-black opacity-60 z-10" />
+      {/* Content */}
+      <div className="relative z-20">
+        <ContainerScroll
+          titleComponent={
+            <>
+              <h1 className="text-4xl font-semibold text-black dark:text-white">
+                Meet The Cloud Legends <br />
+                <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                  AWS CLOUD TEAM
+                </span>
+              </h1>
+            </>
+          }
+        >
+          <img
+            src="https://media.licdn.com/dms/image/v2/D5622AQHmQhYAeIU_Bg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1719095175154?e=1759363200&v=beta&t=wBK1o_CRdQ-DHIkCsv6ecst0NsRlla-Y2uxX9-AodJg"
+            alt="hero"
+            height={720}
+            width={1400}
+            className="mx-auto rounded-2xl object-cover h-full object-left-top"
+            draggable={false}
+          />
+        </ContainerScroll>
+      </div>
     </div>
   );
 }
